a6a3121308c86dc993384be7ecbd1eb03c952135
suemi994 <541253501@qq.com>
2014-12-23 20:15:10 +0800

    test for delete method passed

---
+++ test/requestMethods/delete.js
@@ -0,0 +1,52 @@
+var Zeta=require('../../'),
+    should=require('chai').should(),
+    request=require('supertest'),
+    assert=request('assert');
+var demo=Zeta.module('demo',[]);
+demo.load();
+describe('demo.get',function(){
+    it('should handle the get request',function(done){
+        demo.handler('h1',function($scope){
+            console.log('hhh');
+            $scope.res.writeHead(200,{'Content-Type':'text/plain'});
+            $scope.res.write('DELETE');
+            $scope.res.end();
+        });
+        demo.delete('/foo','h1');
+        request(demo.server()).
+            delete('/foo').
+            expect(200).
+            end(function(err,res){
+                if(err) done(err);
+                res.text.should.include('DELETE');
+                done();
+            });
+    });
+    it('should discard other requests',function(done){
+        request(demo.server()).
+            head('/foo').
+            expect(404,done);
+    });
+    it('should decline the wrong path',function(done){
+        request(demo.server()).
+            delete('/test').
+            expect(404,done);
+    });
+    it('should support dynamic routes',function(done){
+        demo.handler('h1',function($scope){
+            console.log($scope.req.params);
+            $scope.res.writeHead(200,{'Content-Type':'text/plain'});
+            $scope.res.write($scope.req.params.foo);
+            $scope.res.end();
+        });
+        demo.delete('/users/:foo','h1');
+        request(demo.server(true)).
+            delete('/users/test').
+            expect(200).
+            end(function(err,res){
+                if(err) done(err);
+                res.text.should.include('test');
+                done();
+            });
+    });
+});
