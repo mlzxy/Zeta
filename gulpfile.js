3caf03ef4c8a77eb003709956339404dd3d40bcf
Xinyu <bevis@mail.ustc.edu.cn>
2014-12-28 14:10:08 +0800

    now gulp test as whole successfully

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,13 @@ var testsrc = [
     './test/base/testNpmModule.js',
     './test/req.methods/*.js',
     './test/routing/*.js',
-    './test/config/*.js'
+    './test/config/*.js',
+    './test/service.built.in/*.js',
+    './test/error.handle/async.error.js',
+    './test/error.handle/guard.with.syntax.js',
+    './test/error.handle/sync.error.js',
+    './test/service.built.in/static-server/static.js',
+    './test/service/*.js'
 ];
 
 
