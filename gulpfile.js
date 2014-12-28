fbd4529962bf5606fc74a673216f2a73c452e596
Xinyu <bevis@mail.ustc.edu.cn>
2014-12-28 18:10:07 +0800

    add code coverage reporter

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
-var testsrc = [
+var coveralls = require('gulp-coveralls');
+
+
+var testSrc = [
     './test/base/testModule.js',
     './test/base/testCircularModule.js',
     './test/base/testNpmModule.js',
@@ -15,9 +19,31 @@ var testsrc = [
     './test/service/*.js'
 ];
 
+var zetaSrc = [
+    './base/*.js',
+    './modules/**/*.js',
+    './modules/**/**/*.js',
+    './modules/**/**/**/*.js'
+];
+
 
 
 gulp.task('test', function() {
-    gulp.src(testsrc).
+    gulp.src(testSrc).
     pipe(mocha());
+});
+
+
+gulp.task('cov', function(cb) {
+    gulp.src(zetaSrc)
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function() {
+            gulp.src(testSrc)
+                .pipe(mocha())
+                .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+                .on('end', cb);
+        });
+    gulp.src('./coverage/lcov.info')
+        .pipe(coveralls());
 });
