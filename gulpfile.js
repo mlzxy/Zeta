1d7957a0849e8f8117d69fb6489988ce1394e6d0
Xinyu <bevis@mail.ustc.edu.cn>
2014-12-28 14:04:56 +0800

    change gulpfile, try to test as a whole, so far so good

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var testsrc = [
     './test/base/testModule.js',
     './test/base/testCircularModule.js',
     './test/base/testNpmModule.js',
+    './test/req.methods/*.js',
+    './test/routing/*.js',
+    './test/config/*.js'
 ];
 
 
